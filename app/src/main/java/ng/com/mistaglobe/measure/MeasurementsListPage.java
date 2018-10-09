5ad40c91040e3954acbc7e1356857e693a84d17b
Jamiu Okanlawon <Jamiu.Okanlawon@ng.ey.com>
2018-10-09 19:37:57 +0100

    Improved the Bottom Navigation Bar

--- app/src/main/java/ng/com/mistaglobe/measure/MeasurementsListPage.java
+++ app/src/main/java/ng/com/mistaglobe/measure/MeasurementsListPage.java
@@ -1,44 +1,68 @@
 package ng.com.mistaglobe.measure;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MeasurementsListPage extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_recents:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_favorites:
-                    mTextMessage.setText(R.string.favorites);
-                    return true;
-                case R.id.navigation_all:
-                    mTextMessage.setText(R.string.recents);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private BottomNavigationView measurementsBottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurements_list_page);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setupBottomNavigation();
+
+        if(savedInstanceState == null) {
+            loadAllFragment();
+        }
     }
 
+    private void setupBottomNavigation() {
+        measurementsBottomNavView = findViewById(R.id.bottom_navigation);
+
+        measurementsBottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.navigation_recents:
+                        loadRecentsFragment();
+                        return true;
+                    case R.id.navigation_favorites:
+                        loadFavoritesFragment();
+                        return true;
+                    case R.id.navigation_all:
+                        loadAllFragment();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void loadRecentsFragment() {
+        RecentsFragment fragment = RecentsFragment.newInstance();
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame, fragment);
+        ft.commit();
+    }
+
+    private void loadFavoritesFragment() {
+        FavoritesFragment fragment = FavoritesFragment.newInstance();
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame, fragment);
+        ft.commit();
+    }
+
+    private void loadAllFragment() {
+        AllFragment fragment = AllFragment.newInstance();
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame, fragment);
+        ft.commit();
+    }
 }
